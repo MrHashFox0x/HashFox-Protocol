cd9d278a953d6680a3aeb1a88ceb1a9116b09849
MrHashFox0x <MrHashFox0x@outlook.fr>
2025-04-01 00:28:07 +0200

    Update HashFoxDAO.sol

--- HashFoxDAO.sol
+++ HashFoxDAO.sol
@@ -2,16 +2,14 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./GovernanceToken.sol"; // Importation du Token de Gouvernance
+import "./GovernanceToken.sol"; // Governance token import
 
-contract HashFoxDAO is Ownable {
-    using SafeMath for uint256;
+contract HashFoxDAO is Ownable(msg.sender) {
 
     GovernanceToken public governanceToken;
     uint256 public proposalCount;
 
-    uint256 public quorumPercentage = 10; // Quorum de 10% de la supply totale des tokens
+    uint256 public quorumPercentage = 10; // Quorum 10% total supply
 
     struct Proposal {
         string description;
@@ -32,7 +30,7 @@ contract HashFoxDAO is Ownable {
         governanceToken = GovernanceToken(_governanceToken);
     }
 
-    /// @notice Seuls les détenteurs de HFX peuvent créer une proposition
+    /// @notice Only HFX members can add proposal
     function createProposal(string memory _description, uint256 _duration) external {
         require(governanceToken.balanceOf(msg.sender) > 0, "You need to have HFX");
         require(_duration > 0, "Duration invalid");
@@ -44,30 +42,36 @@ contract HashFoxDAO is Ownable {
         emit ProposalCreated(proposalCount - 1, _description, newProposal.endTime);
     }
 
-    /// @notice Vote basé sur les tokens HFX détenus ou stakés
+    function getProposalEndTime(uint256 _proposalId) external view returns (uint256) {
+    return proposals[_proposalId].endTime;
+}
+
+
+
+    /// @notice Vote based on HFX held
     function vote(uint256 _proposalId) external {
         Proposal storage proposal = proposals[_proposalId];
 
         require(block.timestamp < proposal.endTime, "Vote ended");
         require(!proposal.voted[msg.sender], "Already voted");
 
-        uint256 votingPower = governanceToken.getVotingPower(msg.sender);
+        uint256 votingPower = governanceToken.getQuadraticVotingPower(msg.sender);
         require(votingPower > 0, "You need to have HFX token");
 
-        proposal.voteCount = proposal.voteCount.add(votingPower);
+        proposal.voteCount += votingPower;  
         proposal.voted[msg.sender] = true;
 
         emit VoteCasted(msg.sender, _proposalId, votingPower);
     }
 
-    /// @notice Exécuter une proposition si elle a assez de votes et atteint le quorum
+    /// @notice Exécute a proposal if quorum is ok
     function executeProposal(uint256 _proposalId) external {
         Proposal storage proposal = proposals[_proposalId];
 
         require(block.timestamp >= proposal.endTime, "Vote not ended");
         require(!proposal.executed, "Already executed");
 
-        uint256 quorum = governanceToken.totalSupply().mul(quorumPercentage).div(100); // Quorum en pourcentage des tokens totaux
+        uint256 quorum = governanceToken.totalSupply() * quorumPercentage / 100;  // Calcul du quorum sans SafeMath
         require(proposal.voteCount >= quorum, "Quorum not reached");
 
         proposal.executed = true;
@@ -75,7 +79,7 @@ contract HashFoxDAO is Ownable {
         emit ProposalExecuted(_proposalId);
     }
 
-    /// @notice Annuler une proposition si elle n'a pas encore été exécutée
+    /// @notice cancel a proposal if not executed yet
     function cancelProposal(uint256 _proposalId) external onlyOwner {
         Proposal storage proposal = proposals[_proposalId];
         require(!proposal.executed, "Proposal already executed");
@@ -85,7 +89,7 @@ contract HashFoxDAO is Ownable {
         emit ProposalCancelled(_proposalId);
     }
 
-    /// @notice Permet de modifier le quorum en fonction des besoins
+    /// @notice Modify quorum
     function setQuorumPercentage(uint256 _quorumPercentage) external onlyOwner {
         require(_quorumPercentage > 0 && _quorumPercentage <= 100, "Invalid quorum percentage");
         quorumPercentage = _quorumPercentage;
