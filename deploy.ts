34719254176f2b9b06852db4fa59a8e9e0b98bf4
MrHashFox0x <MrHashFox0x@outlook.fr>
2025-04-22 12:07:47 +0200

    Update deploy.ts

--- deploy.ts
+++ deploy.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // Récupérer le compte qui déploie les contrats
+  
   const [deployer] = await ethers.getSigners();
   const stablecoinAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
   const investoraddress = "0xbA93A83d5158A07628c1B9cE7458db4F2bb33e7d";
@@ -10,13 +10,13 @@ async function main() {
 
 
 
-  // Déploiement du contrat Vault 
+
   const VaultFactory = await ethers.getContractFactory("Vault");
   const vault = await VaultFactory.deploy(stablecoinAddress);
   await vault.waitForDeployment();
   console.log(`Vault deployed at: ${await vault.getAddress()}`);
 
-  // Déploiement du contrat MainProject (en supposant qu'il n'a pas besoin de paramètres)
+
   const MainProjectFactory = await ethers.getContractFactory("MainProject");
   const mainProject = await MainProjectFactory.deploy();
   await mainProject.waitForDeployment();
@@ -29,7 +29,7 @@ async function main() {
 
 }
 
-// Exécuter le script avec gestion des erreurs
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
