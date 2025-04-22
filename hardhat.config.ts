f5fbcb27bea6503ffb71dcbda249ce2bb18e1d2b
MrHashFox0x <MrHashFox0x@outlook.fr>
2025-04-22 12:07:06 +0200

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200, // Nombre de fois que le contrat sera optimisé
+      },
+      viaIR: true, // Active l'utilisation de IR pour éviter l'erreur de pile trop profonde
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL!,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
+};
+
+export default config;
+
